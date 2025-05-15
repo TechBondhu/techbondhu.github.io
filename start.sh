8f3060a284643b08ef77944191c2c172e8564b8a
Formbondhu <meta500681@gmail.com>
2025-05-15 11:58:57 +0600

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# শুধু Rasa এবং Action Server চালাও
-rasa run --enable-api --cors "*" --port 5005 --model /app/models/ &
-rasa run actions &
-# Flask সার্ভার চালাও
-cd app && python flask_app.py
